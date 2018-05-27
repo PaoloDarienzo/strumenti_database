a5c3cd644b68097dd669bbab4127297211ffcdc7
Paolo <PaoloDarienzo@users.noreply.github.com>
2018-05-27 09:46:32 +0200

    Add files via upload

---
+++ strumenti_database_eliminazione.sql
@@ -0,0 +1,15 @@
+﻿DROP TABLE IF EXISTS cliente CASCADE;
+
+DROP TABLE IF EXISTS strumento CASCADE;
+
+DROP TABLE IF EXISTS metododipagamento CASCADE;
+
+DROP TABLE IF EXISTS aggiunto;
+DROP TABLE IF EXISTS classificazione;
+DROP TABLE IF EXISTS foto;
+DROP TABLE IF EXISTS marca;
+DROP TABLE IF EXISTS ordine;
+DROP TABLE IF EXISTS puntodiconsegna;
+
+DROP DOMAIN livello;
+DROP DOMAIN tipocliente;
